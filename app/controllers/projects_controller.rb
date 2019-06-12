71329796518075fdfa6ef0aad2f67450a94b90fb
Iris <iris.wolfisberg@hotmail.fr>
2019-06-12 17:12:03 +0200

    Add resume to banner2

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,12 @@ class ProjectsController < ApplicationController
   def homepage
     @projects = Project.all
   end
+
+  def download_pdf
+    send_file(
+      "#{Rails.root}/public/data/resume_iris_wolfisberg.pdf",
+      filename: "resume_IW.pdf",
+      type: "application/pdf"
+    )
+  end
 end
