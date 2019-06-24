2ff6dc59f85abfdc768d8d37bb1803065cb81617
Iris <iris.wolfisberg@hotmail.fr>
2019-06-24 15:15:07 +0200

    Resized and changed images format for faster download

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,19 +15,19 @@ end
 
 crazy_cocktails = create_projects("Crazy Cocktails",
   "A small Ruby on Rails project that allows users to create their own cocktails to add to a public repository. This was an individual project.",
-  "https://res.cloudinary.com/dotvqg92r/image/upload/c_scale,w_400/v1560342929/CrazyCocktails_mzc02k.png",
+  "https://res.cloudinary.com/dotvqg92r/image/upload/c_scale,w_340/v1560929016/sxl69ltfqjtv4bkt0ikk.webp",
   "https://github.com/iwolfisberg/crazy-cocktails",
   "https://crazycocktails.herokuapp.com/",
   "Un simple projet Ruby on Rails permettant aux utilisateurs de créer des recettes de cocktail et de les ajouter à un répertoire public. Ceci est un projet individuel.")
 slowtraveller = create_projects("SlowTraveller",
   "A team project that was developed during Le Wagon Lausanne. SlowTraveller is an app for people that want to find their next destinations and are conscious of their carbon footprint while travelling.",
-  "https://res.cloudinary.com/dotvqg92r/image/upload/c_scale,w_400/v1560342929/SlowTraveller_jld4tm.png",
+  "https://res.cloudinary.com/dotvqg92r/image/upload/c_scale,w_340/v1560929016/g2xjiiwfuymsndgd0owg.webp",
   "https://github.com/iwolfisberg/slowtraveller",
   "http://slowtraveller.xyz/",
   "Un projet développé en équipe lors des deux dernières semaines du Wagon Lausanne. SlowTraveller est une app pour les voyageurs désireux de trouver la prochaine étape de leur voyage et de s'y rendre avec la plus faible empreinte carbone possible.")
 rent_a_cat = create_projects("Rent A Cat",
   "A clone of Airbnb, this Ruby on Rails application was a team project that was developed during Le Wagon Lausanne. *This is a fictionnal project.",
-  "https://res.cloudinary.com/dotvqg92r/image/upload/c_scale,w_400/v1560342930/RentACat_cj1je1.png",
+  "https://res.cloudinary.com/dotvqg92r/image/upload/c_scale,w_340/v1560929017/b8hy64msutyl9h1qrm8f.webp",
   "https://github.com/iwolfisberg/rent-a-cat",
   "https://rent-a-cat.herokuapp.com/",
   "Clone de AirBnB, cette application Ruby on Rails est un projet développé en équipe lors du Wagon Lausanne. *Ceci est bien sûr un projet fictionnel.")
